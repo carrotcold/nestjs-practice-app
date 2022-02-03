21efdd9e3d7fb51757edba40ac56cff875c422f8
Soin Na <thunder.bolt@kakaostyle.com>
2022-02-03 23:27:55 +0900

    feat: create board

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './interfaces/board.interface';
 
@@ -15,5 +15,13 @@ export class BoardsController {
   @Get()
   getAll(): Board[] {
     return this.boardsService.getAll();
+  }
+
+  @Post()
+  create(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Board {
+    return this.boardsService.create(title, description);
   }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './interfaces/board.interface';
+import { Board, BoardStatus } from './interfaces/board.interface';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
@@ -7,5 +8,17 @@ export class BoardsService {
 
   getAll(): Board[] {
     return this.boards;
+  }
+
+  create(title: string, description: string): Board {
+    const newBoard: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(newBoard);
+    return newBoard;
   }
 }
