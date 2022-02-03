6e95edd8601c8a405303e982eaecd131039fa702
Soin Na <thunder.bolt@kakaostyle.com>
2022-02-04 02:04:02 +0900

    faet: sign up

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,11 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './entities/user.entity';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async createUser({ username, password }: AuthCredentialsDto): Promise<void> {
+    const newUser = this.create({ username, password });
+    await this.save(newUser);
+  }
+}
