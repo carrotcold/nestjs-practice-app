b6c5278a4b1657c155127ae740a23881c823273a
Soin Na <thunder.bolt@kakaostyle.com>
2022-02-07 04:20:19 +0900

    feat: hash password with bcryt

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -3,13 +3,17 @@ import {
   InternalServerErrorException,
 } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './entities/user.entity';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser({ username, password }: AuthCredentialsDto): Promise<void> {
-    const newUser = this.create({ username, password });
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = this.create({ username, password: hashedPassword });
 
     try {
       await this.save(newUser);
